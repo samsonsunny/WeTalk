eedc6486a0fe9fd5fea0d953147979cafc53b5eb
samson <sam.sunny@a-cti.com>
2017-08-01 17:43:08 +0530

    Initial Commit

---
+++ WeTalk/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeTalk
+//
+//  Created by Sam on 01/08/17.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
